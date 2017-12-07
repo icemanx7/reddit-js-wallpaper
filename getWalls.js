8b41f4bd81dab7cd49aa8535500e5b6b7b26d1d1
richard thomas <richard.thomas@localhost.localdomain>
2017-12-07 15:37:04 +0200

    added a promise version of the fetch command

--- getWalls.js
+++ getWalls.js
@@ -1,10 +1,32 @@
 'use strict';
+const fetch = require('node-fetch');
 const snoowrap = require('snoowrap');
 const r = require('./config.js');
-const http = require('https');
+const http = require('http');
+const https = require('https');
 const fs = require('fs');
+const FileReader = require('filereader')
 
 const posts = r.getSubreddit('wallpapers').getHot({limit: 900})
+
+const dir = '/home/richard.thomas/Pictures/';
+
+// const testDest = (dir) => {
+//   if (!fs.existsSync(dir)){
+//     fs.mkdirSync(dir);
+//   }
+// }
+
+// const getDestStatus = (dir) => {
+
+// }
+
+// console.log(testDest(dir))
+
+const makeFinal = (temp) => {
+  let name = getBaseName(temp)
+  let final = dir + name
+}
 
 const getHd = (post) => (post.title).includes("1920x1080")
 
@@ -17,26 +39,17 @@ const getBaseName  = (link) => {
   return result
 }
 
-console.log( getBaseName('http://i.pi.gy/LNbJl.png'))
-
 const valid = posts.filter(getHd)
   .map(post => post.url)
   .map(post => { 
     let name = getBaseName(post)
     let file = makeFile(String(name))
-    request(String( post ),file)
+    request(String(post),name)
   })
 
+const request = (url,filename) => fetch(url).then(image => image.body
+  .pipe(fs.createWriteStream(filename))
+  .on('close', () => console.log('image downloaded'))
+);
 
-// const valids = posts.filter(getHd)
-//   .map(post => {
-//     let file = post.url
-//     request(file,file)
-//   })
-//   .then(console.log)
 
-var request = (url,filename) => http.get(url, function(response) {
-  response.pipe(filename);
-});
-
-request('https://i.redd.it/ol38n959lfyz.jpg',makeFile( "r.jpg" ))
